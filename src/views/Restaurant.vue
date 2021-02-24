ee94992a9fa138b3b6a335e6fdd66daeeec45f4e
ihsanmohamad <ihsanmohamad13@gmail.com>
2021-02-24 19:06:26 +0800

    fixed bug

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -13,7 +13,7 @@
             </div>
             <!-- <a :href="state.data.result.website" target="_blank"><svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="w-8 h-8 "><path fill="#3b5998" d="M400 32H48A48 48 0 0 0 0 80v352a48 48 0 0 0 48 48h137.25V327.69h-63V256h63v-54.64c0-62.15 37-96.48 93.67-96.48 27.14 0 55.52 4.84 55.52 4.84v61h-31.27c-30.81 0-40.42 19.12-40.42 38.73V256h68.78l-11 71.69h-57.78V480H400a48 48 0 0 0 48-48V80a48 48 0 0 0-48-48z"></path></svg></a> -->
             
-            <p v-if="state.data.result.opening_hours.open_now === true" class="px-2 py-1 bg-green-500 text-gray-100 font-bold rounded">Open</p>
+            <p v-if="state.data?.result.opening_hours.open_now === true" class="px-2 py-1 bg-green-500 text-gray-100 font-bold rounded">Open</p>
             <p v-else class="px-2 py-1 bg-red-500 text-gray-100 font-bold rounded">Closed</p>
         
             
